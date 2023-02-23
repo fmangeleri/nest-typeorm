2c472033a759996c9a66ce3adac94906842b5706
Franco <fmangeleri@gmail.com>
2023-02-23 19:28:28 -0300

    docker-containers-postgres-pg-db-connection

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,25 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config/dist';
 import config from './config';
+import { Client } from 'pg';
 @Injectable()
 export class AppService {
   constructor(
     @Inject('Tasks') private tasks: any[],
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
+    @Inject('PG') private client: Client,
   ) {}
   getHello(): string {
     return `Hello World! ${this.configService.database.name}`;
+  }
+  getTasks() {
+    return new Promise((resolve, reject) => {
+      this.client.query('SELECT * FROM tasks', (err, res) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(res.rows);
+      });
+    });
   }
 }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,18 @@
 import { Module, Global } from '@nestjs/common';
+import { Client } from 'pg';
 
 const API_KEY = '23r3485u3459342';
 const API_KEY_PROD = 'wdsncjfedb3294898234';
+
+const client = new Client({
+  user: 'root',
+  host: 'localhost',
+  database: 'my_db',
+  password: '123456',
+  port: 5432,
+});
+
+client.connect();
 
 @Global()
 @Module({
@@ -10,7 +21,11 @@ const API_KEY_PROD = 'wdsncjfedb3294898234';
       provide: 'apiKey',
       useValue: process.env.NODE_ENV === 'production' ? API_KEY_PROD : API_KEY,
     },
+    {
+      provide: 'PG',
+      useValue: client,
+    },
   ],
-  exports: ['apiKey'],
+  exports: ['apiKey', 'PG'],
 })
 export class DatabaseModule {}
